43f208ea757cab244257c458e81f6c0c033341a9
Julio Errecart <julioer@me.com>
2023-12-19 10:41:49 -0300

    Fix some comments

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,15 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# Udemy Rails: Mailgun (mailing add-on for Heroku) configuration
+# Udemy Rails:  Mailgun (add-on de mailing para Heroku), configuracion inicial.-
+#               Las variables de entorno las cargó el instalador de Mailgun directo en Heroku.-
+#               La URL es la del deply de esta app en Heroku (produccion).-
 ActionMailer::Base.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'photo-app-je-d9cfce362fe9.herokuapp.com/',
+  :domain         => 'photo-app-je-d9cfce362fe9.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
